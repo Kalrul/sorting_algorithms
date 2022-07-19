cda2e6088fb0e1dd4acaebcbc076d63222bde7cd
Kalrul <prizsabertler@gmail.com>
2022-07-19 15:35:55 +0300

    my header file

--- sort.h
+++ sort.h
@@ -1,9 +1,9 @@
-#ifndef _SORTING_H_
-#define _SORTING_H_
+#ifndef SORT_H
+#define SORT_H
+
 #include <stdio.h>
-
-
-/** Struct Double Linked List */
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,17 +19,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/** Given FUN prototype */
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/** TASK Prototype */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
 
 #endif
